46413474f0cce74c0a12c6155c445dbe395c575b
Rathan13 <rathansracharya466@gmail.com>
2023-10-25 20:39:07 +0530

    set matrix zero

---
+++ setMatrixZero.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h> 
+using namespace std;
+vector<vector<int>> zeroMatrix(vector<vector<int>> &matrix, int n, int m) {
+	// Write your code here.
+        int col[m] = {0};
+        int row[n] = {0};
+    
+
+	    for(int i=0;i<n;i++)
+		{
+			for(int j=0;j<m;j++)
+			{
+				if(matrix[i][j]==0)
+				{
+					col[i]=1;
+					row[j]=1;
+				}
+			}
+		}
+		  for(int i=0;i<n;i++)
+		{
+			for(int j=0;j<m;j++)
+			{
+				 
+
+				 if(col[i]||row[j]==1)
+				 {
+					 matrix[i][j]=0;
+				 }
+			}
+		}
+  return matrix;
+
+
+}
+
+
+
+int main(){
+    vector<vector<int>> arr={{1,2,3},{0,1,2},{1,0,4}};
+    int m=3,n=3;
+    vector<vector<int>> result=zeroMatrix(arr,n,m);
+    for(int i=0;i<n;i++){
+        for(int j=0;i<m;j++){
+            cout<<result[i][j]<<" ";
+
+        }
+        cout<<" "<<endl;
+    }
+}

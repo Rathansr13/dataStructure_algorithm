d874355902a0fb56356fe90d42857d4a19394303
rathansr13 <rathansracharya466@gmail.com>
2023-10-26 21:27:49 +0530

    new 3sum commit

---
+++ 3Sum.cpp
@@ -0,0 +1,39 @@
+
+#include<bits/stdc++.h>
+
+vector<vector<int>> triplet(int n, vector<int> &arr)
+{
+    // Write your code here.
+
+
+    set<vector<int>> st;
+   
+    for (int i = 0; i < n; i++) {
+        set<int> hashset;
+        for (int j = i + 1; j < n; j++) {
+            //Calculate the 3rd element:
+            int third = -(arr[i] + arr[j]);
+
+            //Find the element in the set:
+            if (hashset.find(third) != hashset.end()) {
+                vector<int> temp = {arr[i], arr[j], third};
+                sort(temp.begin(), temp.end());
+                st.insert(temp);
+            }
+            hashset.insert(arr[j]);
+        }
+    }
+
+    //store the set in the answer:
+    vector<vector<int>> ans(st.begin(), st.end());
+    return ans;
+
+}
+
+int main()
+{
+    vector<int> arr={1,8,102,2,3,104,102,103};
+   vector<vector<int>> result=triplet(arr.size(),arr);
+    cout<<result<<endl;
+    
+}
